6b2142e0b44ee6e0e342336e5b0f13e40df23b64
Rameez Kamran <rameezkamran01@gmail.com>
2026-03-08 23:25:04 +0500

    Create Bipartite.cpp

---
+++ Graphs/Bipartite.cpp
@@ -0,0 +1,15 @@
+bool bipartite(int u, int curr, vvi &adj, vi &color){
+    color[u]=curr;
+
+    for(auto &v : adj[u]){
+        if(color[v] == -1){
+            if(!bipartite(v,curr^1, adj, color)){
+                return 0;
+            }
+        }
+        else if(color[v] == color[u]){
+            return 0;
+        }
+    }
+    return 1;
+}
