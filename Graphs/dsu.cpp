834955971d2b1040bcbf475e775c4cce2eb815eb
Rameez Kamran <rameezkamran01@gmail.com>
2026-08-26 14:59:31 +0500

    Update dsu.cpp

--- Graphs/dsu.cpp
+++ Graphs/dsu.cpp
@@ -1,32 +1,57 @@
-class UnionFind{
-    vector<int>parent;
-    vector<int>rank;
-    public:
-        UnionFind(int n){
-            parent.resize(n);
-            iota(parent.begin(), parent.end(), 0);
-            rank.resize(n);
+class UnionFind
+{
+public:
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> sz;
+    UnionFind(int n)
+    {
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        rank.resize(n);
+        sz.resize(n, 1);
+    }
+    int find(int i)
+    {
+        if (i == parent[i])
+        {
+            return i;
         }
-        int find(int i){
-            if(i == parent[i]){
-                return i;
-            }
-            return parent[i] = find(parent[i]);
-        }
+        return parent[i] = find(parent[i]);
+    }
 
-        void unite(int i, int j){
-            int rootI = find(i), rootJ = find(j);
-            if(rootI != rootJ){
-                if(rank[rootI] > rank[rootJ]){
-                    parent[rootJ] = rootI;
-                } 
-                else if(rank[rootI] < rank[rootJ]){
-                    parent[rootI] = rootJ;
-                }
-                else{
-                    parent[rootI] = rootJ;
-                    rank[rootJ]++;
-                }
+    void unite_rank(int i, int j)
+    {
+        int rootI = find(i), rootJ = find(j);
+        if (rootI != rootJ)
+        {
+            if (rank[rootI] > rank[rootJ])
+            {
+                parent[rootJ] = rootI;
+            }
+            else if (rank[rootI] < rank[rootJ])
+            {
+                parent[rootI] = rootJ;
+            }
+            else
+            {
+                parent[rootI] = rootJ;
+                rank[rootJ]++;
             }
         }
+    }
+    void unite_size(int i, int j)
+    {
+        i = find(i);
+        j = find(j);
+
+        if (i == j)
+            return;
+
+        if (sz[i] < sz[j])
+            swap(i, j);
+
+        parent[j] = i;
+        sz[i] += sz[j];
+    }
 };
